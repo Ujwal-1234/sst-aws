5540ffdb409a14fa9f64b2d328833b75c9a86ff4
UJWAL <kujwal147@gmail.com>
2023-01-23 18:32:43 +0530

    Storage stack with bucket

---
+++ stacks/storageStack.js
@@ -0,0 +1,16 @@
+import {Table} from "@serverless-stack/resources";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+export function StorageStack({stack, app}){
+    const bucket = new Bucket(stack, "Uploads")
+    const table = new Table(stack, "Notes", {
+        fields:{
+            userId:"string",
+            noteId:"string"
+        },
+        primaryIndex:{
+            partitionKey:"userId", sortKey:"noteId"
+        },
+
+    })
+    return ({table, bucket})
+}
