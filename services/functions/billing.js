f0c1aedd2fd8bdcaf2dec1442631de755826f847
Ujwal Kumar Mahato <kujwal147@gmail.com>
2023-01-25 12:09:31 +0530

    stripe working

--- services/functions/billing.js
+++ services/functions/billing.js
@@ -7,13 +7,24 @@ export const main = handler(async (event) => {
   const amount = calculateCost(storage);
   const description = "Scratch charge";
 
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-  await stripe.charges.create({
-    source,
-    amount,
+  const customer = await stripe.customers.create({
+    // source,
+    // amount,
+    name:'abcd',
     description,
-    currency: "usd",
+    // currency: "usd",
+    shipping: {
+      name: "Jenny Rosen",
+      address: {
+        line1: "510 Townsend St",
+        postal_code: "98140",
+        city: "San Francisco",
+        state: "CA",
+        country: "US",
+      },
+    },
   });
 
   return { status: true };
